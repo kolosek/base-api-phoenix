821bb84ff809292e3b104d15d1fbe15521e43c7a
Predrag <predrag@kolosek.com>
2017-10-12 10:13:18 +0200

    Generate create_user migration

---
+++ priv/repo/migrations/20171012080646_create_user.exs
@@ -0,0 +1,14 @@
+defmodule CompanyApi.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :varchar
+      add :subname, :varchar
+      add :email, :varchar
+      add :job, :varchar 
+
+      timestamps()
+    end
+  end
+end
