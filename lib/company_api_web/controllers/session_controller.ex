cfe1c70891a5a9e86836a79b54d556a7eb29308c
Predrag <predrag@kolosek.com>
2017-10-17 12:19:10 +0200

    Add Channel users test

--- lib/company_api_web/controllers/session_controller.ex
+++ lib/company_api_web/controllers/session_controller.ex
@@ -7,9 +7,9 @@ defmodule CompanyApiWeb.SessionController do
     case User.check_registration(Map.new(params, fn {k, v} -> {String.to_atom(k), v} end)) do
       {:ok, user} ->
         new_conn = Guardian.Plug.sign_in(conn, CompanyApi.Guardian, user)
-        token = Guardian.Plug.current_token(new_conn)
-        claims = Guardian.Plug.current_claims(new_conn)
-        expire = Map.get(claims, "exp")
+        token    = Guardian.Plug.current_token(new_conn)
+        claims   = Guardian.Plug.current_claims(new_conn)
+        expire   = Map.get(claims, "exp")
 
         new_conn
         |> put_resp_header("authorization", "Bearer #{token}")
